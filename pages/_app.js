f3161b433ab60d08a26525d2e81ccc483809d2e3
Dhruv Gupta <dhruv035@gmail.com>
2023-09-18 18:45:07 +0530

    Error

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,7 @@
 import '@/styles/globals.css'
 import { ChakraProvider } from '@chakra-ui/react'
 import Fonts from './fonts'
-import theme from './theme'
+import theme from '../theme.js'
 export default function App({ Component, pageProps }) {
  
   return  (<ChakraProvider theme={theme}>
