07543bdee2ad872a012bac7a33cac6329741b160
Dhruv Gupta <dhruv035@gmail.com>
2023-09-22 19:44:29 +0530

    Added

---
+++ components/logo.js
@@ -0,0 +1,30 @@
+import React from "react";
+import useDimensions from "./useDimensions";
+const Logo = ()=>{
+
+    const windowDimensions=useDimensions();
+    const logoScale=0.15;
+    return(
+        <img
+              className="shunya-logo fixed"
+              src={"/images/shunyaLogo.png"}
+              style={{
+                opacity: 0.6,
+                top: "20px",
+                left: "32px",
+                
+                //filter: "hue-rotate(90deg) contrast(160%)",
+                height:
+                  windowDimensions.width > windowDimensions.height
+                    ? (windowDimensions.height * 8) / 10
+                    : (windowDimensions.width * 8) / 10*logoScale,
+                width:
+                  windowDimensions.width > windowDimensions.height
+                    ? (windowDimensions.height * 8) / 10
+                    : (windowDimensions.width * 8) / 10*logoScale,
+                zIndex: 100,
+              }}
+            />
+    )
+}
+export default Logo;
--- pages/ecstatic.js
+++ pages/ecstatic.js
@@ -1,21 +1,37 @@
-import React from "react";
+import React, { useState, useEffect, useRef } from "react";
 import Menu from "@/components/menu";
+import { motion, useMotionValueEvent, useInView } from "framer-motion";
 import { Text } from "@chakra-ui/react";
 import useDimensions from "@/components/useDimensions";
+import Logo from "@/components/logo";
+import { useScroll } from "framer-motion";
 const Ecstatic = () => {
-    const dimensions = useDimensions();
+  const dimensions = useDimensions();
+  const { scrollY, scrollYProgress } = useScroll();
+  const [tracker, setTracker] = useState(true);
+  const [scrolled, setScrolled] = useState(false);
+  const ref = useRef(null);
+  const isInView = useInView(ref, { amount: 0.6});
+
+  const variantsText = {
+    base: { opacity: 0, y: 0, transition: { duration: 1 } },
+    scrolled: { opacity: 1, y: -100, transition: { duration: 1 } },
+  };
+
+  useMotionValueEvent(scrollY, "change", (latest) => {
+    if (!scrolled) setScrolled(true);
+  });
   return (
     <div className="bg-black">
-       <Menu />
-      <main className=" flex-col -mt-6"> 
-      
+      <Menu />
+      <main className=" flex-col -mt-6">
         <div className="flex flex-col items-center bg-black">
-        {/*<Text className="text-[30px] md:text-[60px] text-amber-400 mt-[40px] md:mt-[20px] z-[11]">
+          {/*<Text className="text-[30px] md:text-[60px] text-amber-400 mt-[40px] md:mt-[20px] z-[11]">
             Ecstatic Dance
   </Text>*/}
 
           <div className=" flex overflow-hidden">
-            
+            <Logo />
             <video
               className="z-[10]"
               autoPlay
@@ -24,51 +40,66 @@ const Ecstatic = () => {
               style={{
                 filter: "hue-rotate(120deg)",
                 opacity: 0.7,
-                width:(dimensions.height+80)*16/9,
-                height:dimensions.height+80,
-                top:0,
-                left:0,
-                objectFit:"cover",
+                width: ((dimensions.height + 30) * 16) / 9,
+                height: dimensions.height + 30,
+                top: 0,
+                left: 0,
+                objectFit: "cover",
               }}
             >
-              <source src="./videos/ecstatic-bg.mp4" />
+              <source src="./videos/ecstatic-reel.mp4" />
             </video>
           </div>
-          
-          <div className="w-3/4  ml-10 md:ml-20 self-start">
-            <Text className="text-greenLight relative text-[16px] md:text-[30px] mt-6 opacity-50 z-[100]">
-              Throughout human history, people have practiced some form of dance
-              as a way of altering one’s consciousness and connecting with
-              nature, other, and self. The modern day movement of ecstatic dance
-              brings us back to these ancient practices, with the influence of
-              Gabrielle Roth’s 5Rhythms practice, conscious dance movement
-              meditations, yoga, and somatic therapies flourishing today. It is
-              a free-form dance practice where participants can gather to dance
-              to music without the need of following the instructions of a
-              leader. In Ecstatic Dance the music is the leader and participants
-              are free to weave their way through the different soundscapes.
-              Throughout human history, people have practiced some form of dance
-              as a way of altering one’s consciousness and connecting with
-              nature, other, and self. The modern day movement of ecstatic dance
-              brings us back to these ancient practices, with the influence of
-              Gabrielle Roth’s 5Rhythms practice, conscious dance movement
-              meditations, yoga, and somatic therapies flourishing today. It is
-              a free-form dance practice where participants can gather to dance
-              to music without the need of following the instructions of a
-              leader. In Ecstatic Dance the music is the leader and participants
-              are free to weave their way through the different soundscapes.
-              Throughout human history, people have practiced some form of dance
-              as a way of altering one’s consciousness and connecting with
-              nature, other, and self. The modern day movement of ecstatic dance
-              brings us back to these ancient practices, with the influence of
-              Gabrielle Roth’s 5Rhythms practice, conscious dance movement
-              meditations, yoga, and somatic therapies flourishing today. It is
-              a free-form dance practice where participants can gather to dance
-              to music without the need of following the instructions of a
-              leader. In Ecstatic Dance the music is the leader and participants
-              are free to weave their way through the different soundscapes.
+          <motion.div
+            ref={ref}
+            initial={{ opacity: 0 }}
+            animate={isInView ? "scrolled" : "base"}
+            variants={variantsText}
+            className="w-3/4  ml-10 md:ml-20 self-start z-[20]"
+          >
+            <Text className="text-greenDark text-[32px] mt-[150px]">
+              Ecstatic Dance
             </Text>
-          </div>
+            <Text className="text-greenLight relative text-[24px] md:text-[30px] mt-6 z-[100]">
+              Throughout human history, people have practiced some form of dance
+              as a way of altering one’s consciousness and connecting with
+              nature, other, and self. The modern day movement of ecstatic dance
+              brings us back to these ancient practices, with the influence of
+              Gabrielle Roth’s 5Rhythms practice
+            </Text>
+          </motion.div>
+          <motion.div className="w-3/4  ml-10 md:ml-20 self-start z-[20]">
+          <Text className="text-greenDark text-[32px] mt-[150px]">
+              The Team
+            </Text>
+            <Text className="text-greenDark text-[32px] mt-[150px]">
+              The Team
+            </Text>
+            <Text className="text-greenDark text-[32px] mt-[150px]">
+              The Team
+            </Text>
+            <Text className="text-greenDark text-[32px] mt-[150px]">
+              The Team
+            </Text>
+            <Text className="text-greenDark text-[32px] mt-[150px]">
+              The Team
+            </Text>
+            <Text className="text-greenDark text-[32px] mt-[150px]">
+              The Team
+            </Text>
+            <Text className="text-greenDark text-[32px] mt-[150px]">
+              The Team
+            </Text>
+            <Text className="text-greenDark text-[32px] mt-[150px]">
+              The Team
+            </Text>
+            <Text className="text-greenDark text-[32px] mt-[150px]">
+              The Team
+            </Text>
+            <Text className="text-greenDark text-[32px] mt-[150px]">
+              The Team
+            </Text>
+          </motion.div >
         </div>
       </main>
     </div>
