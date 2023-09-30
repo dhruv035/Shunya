1b28e1f5d5c7e2c771444d6605829862f527beb7
Dhruv Gupta <dhruv035@gmail.com>
2023-09-30 20:53:10 +0530

    Changes

--- pages/_document.js
+++ pages/_document.js
@@ -7,15 +7,15 @@ export default function Document() {
         <link rel="preload"
         href="/fonts/La_Belle_Aurore/LaBelleAurore-Regular.ttf"
         as="font"
-        type="font/tff"/>
+        />
          <link rel="preload"
         href="/fonts/Titillium_Web/TitilliumWeb-Regular.ttf"
         as="font"
-        type="font/tff"/>
+       />
          <link rel="preload"
         href="/fonts/Lobster/Lobster-Regular.ttf"
         as="font"
-        type="font/tff"/>
+       />
         
         </Head>
       <body>
